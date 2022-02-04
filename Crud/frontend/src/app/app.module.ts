3742981c36bfb334d7580246f6814a07e5eb9a65
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2022-02-04 16:35:16 -0300

    adicionando o modulo do forms

--- Crud/frontend/src/app/app.module.ts
+++ Crud/frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

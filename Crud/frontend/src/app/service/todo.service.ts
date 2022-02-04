193ef844443ed6135802b1344dd1defae24ad932
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2022-02-04 16:34:41 -0300

    Criando novos metudos para consumo de api

--- Crud/frontend/src/app/service/todo.service.ts
+++ Crud/frontend/src/app/service/todo.service.ts
@@ -10,10 +10,19 @@ import { Observable } from 'rxjs';
 export class TodoService {
 
   constructor(private httpClient: HttpClient) { }
-  url = 'https://localhost:7028/'
+  url = 'https://localhost:7028'
 
+  httpOptions =  {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  }
   getTodo(): Observable<Todo[]>{
     return this.httpClient.get<Todo[]>(`${this.url}/v1/todos`)
+  }
+  postTodo(todo: Todo): Observable<Todo>{
+    return this.httpClient.post<Todo>(`${this.url}/v1/create`,JSON.stringify(todo),this.httpOptions);
+  }
+  deleteTodo(todo: Todo): Observable<Todo>{
+    return this.httpClient.delete<Todo>(`${this.url}/v1/delete/${todo.id}`,this.httpOptions)
   }
 
 }

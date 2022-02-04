111f4e3eca3e2d52e31f73c8e537c382636b6569
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2022-02-04 16:26:54 -0300

    modificando o program.cs para o frontend conseguir acessar e dando um database update

--- Cod3r/backend/Program.cs
+++ Cod3r/backend/Program.cs
@@ -1,15 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var MyAllowSpecificOrigins = "_Angular";
+
 builder.Services.AddDbContext<AppDbContext>();
 
+builder.Services.AddCors(op =>
+{
+    op.AddPolicy(name: MyAllowSpecificOrigins,
+                builder => {
+                    builder.WithOrigins("http://localhost:4200",
+                                        "https://localhost:4200")
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod();
+                });
+});
+
 var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 //app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/v1/todos", (AppDbContext context) => {
     var todos = context.Todos.ToList();
     return Results.Ok(todos);
-});
+}).AllowAnonymous();
 
 app.MapGet("/v1/{id}", async(int id, AppDbContext context) => {
     var dbTodo = await context.Todos.FindAsync(id);
